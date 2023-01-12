298c720690aa5f9f70d5ec59019d8818cab4e25e
Olivier-Kango <olivierkango@gmail.com>
2023-01-12 11:36:04 -0800

    Create class Person

---
+++ person.rb
@@ -0,0 +1,34 @@
+require_relative 'nameable'
+
+class Person
+  attr_reader :id
+  attr_accessor :name, :age, :rentals
+
+  def initialize(age, name, parent_permission)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def can_use_services?
+    return unless of_age || @parent_permission
+
+    true
+  end
+
+  def correct_name
+    @name
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
